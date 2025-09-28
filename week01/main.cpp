8d913f0c629bab8a95d409d48f8fea3b3bed5ac7
heshan8 <heshan988@gmail.com>
2025-09-28 16:09:58 +1000

    Add comments for clarity; remove redundant std namespace

--- week01/main.cpp
+++ week01/main.cpp
@@ -1,7 +1,10 @@
+/* Week 1 exercises */
 #include <iostream>
 using namespace std;
 int main() {
-    std::cout << "Week 01 running\n";
+    /* Exercise #1 - a program to print welcome message */
+    cout << "Week 01 running\n"; /* to show that week 1 practical is running */
+    cout << "-----------------\n";
     cout << "Welcome to cp2406 2025" << endl;
     return 0;
 }
