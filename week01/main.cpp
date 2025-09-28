6229ae1cac1236c3d304e19b54ee92f948f2bd26
heshan8 <heshan988@gmail.com>
2025-09-28 17:35:56 +1000

    Fill in if statements for exercise 3

--- week01/main.cpp
+++ week01/main.cpp
@@ -21,6 +21,38 @@ void conversionExample() {
     }
 }
 
+/* Exercise #3 - salary and switch statement
+* Ask the user for their salary and determine their position
+ */
+void findPosition() {
+    float salary;
+    cout << "Enter your salary: ";
+    cin >> salary;
+    int category;
+    if (salary > 60000) { //Engineer
+        category = 1;
+    } else if (salary < 60000 && salary > 80000) { //Senior engineer
+        /*The Only way to say between two numbers is to
+         *combine two comparisons with an operator, which in this case is &&*/
+        category = 2;
+    } else if (salary > 80000) { //Manger
+        category = 3;
+    } else {
+        category = 0;
+    }
+    switch (________) {
+        case 1:
+            cout << "Position type: Engineer" << endl; break;
+        case 2:
+            cout << "Position type: Senior Engineer" << endl; break;
+        case 3:
+            cout << "Position type: Manager" << endl; break;
+            ______:
+            cout << "Invalid position type." << endl;
+    }
+}
+
+
 int main() {
     /* Exercise #1 - a program to print welcome message */
     cout << "Week 01 running\n"; /* to show that week 1 practical is running */
