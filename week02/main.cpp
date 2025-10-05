77ecd81c858d05ae7b4b712ad4526a722330a0bf
heshan8 <heshan988@gmail.com>
2025-10-05 16:16:34 +1000

    Add starter code for exercise 1

--- week02/main.cpp
+++ week02/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+using namespace std;
+
 int main() {
-    std::cout << "Week 02 running\n";
+    cout << "Week 01 practical running\n"; /* to show that week 2 practical is running */
+    cout << "-----------------------------\n";
+    // Exercise #1 - Integer pointer and Indirection
+    int age = 27; // Replace 21 with your actual age
+    int* agePtr = &age;
+    cout << "Address of age: " << agePtr << endl;
+    cout << "Value of age using indirection: " << *agePtr << endl;
+
+
     return 0;
 }
