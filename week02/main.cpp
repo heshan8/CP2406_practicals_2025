96cd3a75db05961b9ddab5a033c278400c4d47c6
heshan8 <heshan988@gmail.com>
2025-10-05 16:29:15 +1000

    Move exercise 1 out of main into its own function

--- week02/main.cpp
+++ week02/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Week 01 practical running\n"; /* to show that week 2 practical is running */
-    cout << "-----------------------------\n";
-    // Exercise #1 - Integer pointer and Indirection
+// Exercise #1 - Integer pointer and Indirection
+void integerPointer() {
     int age = 27; // Replace 21 with your actual age
     int* agePtr = &age;
     cout << "Address of age: " << agePtr << endl;
     cout << "Value of age using indirection: " << *agePtr << endl;
+}
+
+int main() {
+    cout << "Week 02 practical running\n"; /* to show that week 2 practical is running */
+    cout << "-----------------------------\n";
+    // Exercise #1 - Integer pointer and Indirection
+    integerPointer();
 
 
     return 0;
