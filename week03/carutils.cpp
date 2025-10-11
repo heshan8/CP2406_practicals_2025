9d523e79618fb117e936af8c0831da709b0ce036
heshan8 <heshan988@gmail.com>
2025-10-11 22:29:34 +1000

    Move functions to a support file

--- week03/carutils.cpp
+++ week03/carutils.cpp
@@ -2,3 +2,23 @@
 #include "carutils.h"
 using namespace std;
 
+// --- Function Implementation ---
+// Takes in distance and time
+// Returns average speed in km/h
+double calculateSpeed(double distanceKm, double timeHours) {
+    if (timeHours <= 0) {
+        cerr << "Time must be > 0 hours.\n";
+        return 0.0;
+    }
+    return distanceKm / timeHours; // km/h
+}
+
+// Takes in Litres and Distance
+// Returns fuel efficiency in L/100 km
+double fuelEfficiencyLPer100Km(double litresUsed, double distanceKm) {
+    if (distanceKm <= 0) {
+        cerr << "Distance must be > 0 km.\n";
+        return 0.0;
+    }
+    return (litresUsed / distanceKm) * 100.0; // L/100 km
+}
--- week03/main.cpp
+++ week03/main.cpp
@@ -31,23 +31,3 @@ int main() {
     return 0;
 }
 
-// --- Function Implementation ---
-// Takes in distance and time
-// Returns average speed in km/h
-double calculateSpeed(double distanceKm, double timeHours) {
-    if (timeHours <= 0) {
-        cerr << "Time must be > 0 hours.\n";
-        return 0.0;
-    }
-    return distanceKm / timeHours; // km/h
-}
-
-// Takes in Litres and Distance
-// Returns fuel efficiency in L/100 km
-double fuelEfficiencyLPer100Km(double litresUsed, double distanceKm) {
-    if (distanceKm <= 0) {
-        cerr << "Distance must be > 0 km.\n";
-        return 0.0;
-    }
-    return (litresUsed / distanceKm) * 100.0; // L/100 km
-}
