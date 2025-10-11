42711cc8dac84c137ba5c2b28f3bddf4b9115afb
heshan8 <heshan988@gmail.com>
2025-10-11 21:52:42 +1000

    Optimize code structure

--- week03/main.cpp
+++ week03/main.cpp
@@ -4,26 +4,8 @@ using namespace std;
 // Program to calculate average speed and fuel efficiency
 // --- Function Headers ---
 // Also known as "function signatures" or "function prototypes"
-// Takes in distance and time
-// Returns average speed in km/h
-double calculateSpeed(double distanceKm, double timeHours) {
-    if (timeHours <= 0) {
-        cerr << "Time must be > 0 hours.\n";
-        return 0.0;
-    }
-    return distanceKm / timeHours; // km/h
-}
-
-// Takes in Litres and Distance
-// Returns fuel efficiency in L/100 km
-double fuelEfficiencyLPer100Km(double litresUsed, double distanceKm) {
-    if (distanceKm <= 0) {
-        cerr << "Distance must be > 0 km.\n";
-        return 0.0;
-    }
-    return (litresUsed / distanceKm) * 100.0; // L/100 km
-}
-
+double calculateSpeed(double distanceKm, double timeHours);
+double fuelEfficiencyLPer100Km(double litresUsed, double distanceKm);
 
 int main() {
     cout << "Week 03 practical running\n";
@@ -50,4 +32,23 @@ int main() {
     cout << "Fuel efficiency: " << lPer100 << " L/100 km\n";
 
     return 0;
+}
+// Takes in distance and time
+// Returns average speed in km/h
+double calculateSpeed(double distanceKm, double timeHours) {
+    if (timeHours <= 0) {
+        cerr << "Time must be > 0 hours.\n";
+        return 0.0;
+    }
+    return distanceKm / timeHours; // km/h
+}
+
+// Takes in Litres and Distance
+// Returns fuel efficiency in L/100 km
+double fuelEfficiencyLPer100Km(double litresUsed, double distanceKm) {
+    if (distanceKm <= 0) {
+        cerr << "Distance must be > 0 km.\n";
+        return 0.0;
+    }
+    return (litresUsed / distanceKm) * 100.0; // L/100 km
 }
