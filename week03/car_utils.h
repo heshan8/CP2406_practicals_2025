6e51ea2a85e4180d265b7dfe377af1ae05e0ddc0
heshan8 <heshan988@gmail.com>
2025-10-11 21:28:25 +1000

    Move function headers to new file

--- week03/car_utils.h
+++ week03/car_utils.h
@@ -5,4 +5,14 @@
 #ifndef CP2406_PRACTICALS_2025_CAR_UTILS_H
 #define CP2406_PRACTICALS_2025_CAR_UTILS_H
 
+// --- Function Headers ---
+// Also known as "function signatures" or "function prototypes"
+// Takes in distance and time
+// Returns average speed in km/h
+double calculateSpeed(double distanceKm, double timeHours);
+
+// Takes in Litres and Distance
+// Returns fuel efficiency in L/100 km
+double fuelEfficiencyLPer100Km(double litresUsed, double distanceKm);
+
 #endif //CP2406_PRACTICALS_2025_CAR_UTILS_H
