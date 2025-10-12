686c941f272a9ce7f758aa4f8eae7bea712d23f4
heshan8 <heshan988@gmail.com>
2025-10-12 20:11:24 +1000

    Update main.cpp to use new functions

--- week03/main.cpp
+++ week03/main.cpp
@@ -22,11 +22,15 @@ int main() {
     // --- Use the functions ---
     double speedKmh = calculateSpeed(distanceKm, timeHours);
     double lPer100 = fuelEfficiencyLPer100Km(litres, distanceKm);
+    double speedMs = speedInMetersPerSecond(speedKmh);
+    double estC02Emissions = estimatedCO2Emissions(litres);
 
     // --- Print neatly ---
     cout << fixed << setprecision(2); // Format the output to two decimal places
     cout << "Average speed: " << speedKmh << " km/h\n";
+    cout << "Average speed in m/s: " << speedMs << " M/s\n";
     cout << "Fuel efficiency: " << lPer100 << " L/100 km\n";
+    cout << "Estimated C02 efficiency: " << estC02Emissions << " kg\n";
 
     return 0;
 }
