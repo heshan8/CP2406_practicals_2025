0b5972527eebb97507cc7380ba2547dfa3abfcad
heshan8 <heshan988@gmail.com>
2025-10-12 20:49:46 +1000

    Fill in blanks in showMenu() code

--- week03/main.cpp
+++ week03/main.cpp
@@ -19,50 +19,50 @@ int main() {
         showMenu();
         cout << "Enter your choice: ";
         cin >> choice;
-        switch (___) { // Fill in the variable to switch on
-            case ___: { // Option for average speed
+        switch (choice) { // Fill in the variable to switch on
+            case 1: { // Option for average speed (using cases 1-5 for 5 menu options)
                 double distance, time;
                 cout << "Distance (km): ";
                 cin >> distance;
                 cout << "Time (hours): ";
                 cin >> time;
-                double speed = calculateSpeed(___, ___); // Fill in arguments
+                double speed = calculateSpeed(distance, time); // Fill in arguments
                 cout << "Average speed: " << speed << " km/h\n";
-                ___;
+                break;
             }
-            case ___: { // Option for fuel efficiency
+            case 2: { // Option for fuel efficiency
                 double litres, distance;
                 cout << "Litres used: ";
                 cin >> litres;
                 cout << "Distance (km): ";
                 cin >> distance;
-                double lPer100 = fuelEfficiencyLPer100Km(___, ___); // Fill arguments
+                double lPer100 = fuelEfficiencyLPer100Km(litres, distance); // Fill arguments
                 cout << "Fuel efficiency: " << lPer100 << " L/100 km\n";
-                ___;
+                break;
             }
-            case ___: { // Option for km/h to m/s
+            case 3: { // Option for km/h to m/s
                 double speedKmh;
                 cout << "Speed (km/h): ";
                 cin >> speedKmh;
-                double speedMs = speedInMetresPerSecond(___); // Fill in argument
+                double speedMs = speedInMetresPerSecond(speedKmh); // Fill in argument
                 cout << "Speed: " << speedMs << " m/s\n";
-                ___;
+                break;
             }
-            case ___: { // Option for CO2 emissions
+            case 4: { // Option for CO2 emissions
                 double litres;
                 cout << "Litres used: ";
                 cin >> litres;
                 double co2 = estimatedCO2Emissions(litres); // Fill in argument
                 cout << "Estimated CO2 emissions: " << co2 << " kg\n";
-                ___;
+                break;
             }
-            case ___: // Option to quit
+            case 5: // Option to quit
                 cout << "Goodbye!\n";
-                ___;
+                break;
             default:
                 cout << "Invalid option. Please try again.\n";
         }
-    } while (___ ___ ___); // Fill in the quit value
+    } while (choice !=5); // Fill in the quit value
     return 0;
 }
 
