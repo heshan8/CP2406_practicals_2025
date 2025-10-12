b508a3f1ae408f96555416961c405647fa6006e8
heshan8 <heshan988@gmail.com>
2025-10-12 18:41:13 +1000

    Add new exercise 2 functions

--- week03/car_utils.cpp
+++ week03/car_utils.cpp
@@ -22,13 +22,15 @@ double fuelEfficiencyLPer100Km(double litresUsed, double distanceKm) {
     return (litresUsed / distanceKm) * 100.0; // L/100 km
 }
 
-// Returns speed in m/s given speed in km/h
-double ____________(____________ ____________) {
+//----Exercise 2----
+// Returns speed in metres/s given speed in km/h
+double speedInMetersPerSecond(double speedKmh) {
     // 1 km/h = 1000/3600 m/s = 0.27778 m/s
-    return ____________ * ____________;
+    return speedKmh * (1000/3600.0);
 }
+
 // Estimates CO2 emissions (kg) given litres of petrol used
-double ____________(____________ ____________) {
+double estimatedCO2Emissions(double  litres) {
     // Use 2.31 kg CO2 per litre of petrol (standard emission factor)
-    return ____________ * ____________;
+    return litres * 2.31;
 }
