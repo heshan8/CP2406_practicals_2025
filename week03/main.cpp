9e1cbfb2f08281e9826bf723662f9b135bc70415
heshan8 <heshan988@gmail.com>
2025-10-12 20:24:37 +1000

    Replace old code in main with new code to use menu system

--- week03/main.cpp
+++ week03/main.cpp
@@ -14,33 +14,55 @@ void showMenu() {
 }
 
 int main() {
-    cout << "Week 03 practical running\n";
-    cout << "-----------------------------\n";
-    cout << "CP2406 - Week 3: Functions demo\n";
-
-    double distanceKm, timeHours, litres;
-    cout << "Enter distance travelled (km): ";
-    cin >> distanceKm;
-
-    cout << "Enter time taken (hours): ";
-    cin >> timeHours;
-
-    cout << "Enter petrol used (litres): ";
-    cin >> litres;
-
-    // --- Use the functions ---
-    double speedKmh = calculateSpeed(distanceKm, timeHours);
-    double lPer100 = fuelEfficiencyLPer100Km(litres, distanceKm);
-    double speedMs = speedInMetersPerSecond(speedKmh);
-    double estC02Emissions = estimatedCO2Emissions(litres);
-
-    // --- Print neatly ---
-    cout << fixed << setprecision(2); // Format the output to two decimal places
-    cout << "Average speed: " << speedKmh << " km/h\n";
-    cout << "Average speed in m/s: " << speedMs << " M/s\n";
-    cout << "Fuel efficiency: " << lPer100 << " L/100 km\n";
-    cout << "Estimated C02 efficiency: " << estC02Emissions << " kg\n";
-
+    int choice;
+    do {
+        showMenu();
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (___) { // Fill in the variable to switch on
+            case ___: { // Option for average speed
+                double distance, time;
+                cout << "Distance (km): ";
+                cin >> distance;
+                cout << "Time (hours): ";
+                cin >> time;
+                double speed = calculateSpeed(___, ___); // Fill in arguments
+                cout << "Average speed: " << speed << " km/h\n";
+                ___;
+            }
+            case ___: { // Option for fuel efficiency
+                double litres, distance;
+                cout << "Litres used: ";
+                cin >> litres;
+                cout << "Distance (km): ";
+                cin >> distance;
+                double lPer100 = fuelEfficiencyLPer100Km(___, ___); // Fill arguments
+                cout << "Fuel efficiency: " << lPer100 << " L/100 km\n";
+                ___;
+            }
+            case ___: { // Option for km/h to m/s
+                double speedKmh;
+                cout << "Speed (km/h): ";
+                cin >> speedKmh;
+                double speedMs = speedInMetresPerSecond(___); // Fill in argument
+                cout << "Speed: " << speedMs << " m/s\n";
+                ___;
+            }
+            case ___: { // Option for CO2 emissions
+                double litres;
+                cout << "Litres used: ";
+                cin >> litres;
+                double co2 = estimatedCO2Emissions(litres); // Fill in argument
+                cout << "Estimated CO2 emissions: " << co2 << " kg\n";
+                ___;
+            }
+            case ___: // Option to quit
+                cout << "Goodbye!\n";
+                ___;
+            default:
+                cout << "Invalid option. Please try again.\n";
+        }
+    } while (___ ___ ___); // Fill in the quit value
     return 0;
 }
 
